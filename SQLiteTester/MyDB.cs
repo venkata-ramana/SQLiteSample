b4971feffa72434b732a1cd738819a307f1d4c9e
Venkata Ramana Neelapala <venkat@keka.com>
2018-03-21 18:02:08 +0530

    InitialCommit

---
+++ SQLiteTester/MyDB.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLiteTester
+{
+    /// <summary>
+    /// Class MyDb
+    /// </summary>
+    /// <seealso cref="NPoco.Database" />
+    public class MyDb : NPoco.Database
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MyDb"/> class.
+        /// </summary>
+        public MyDb()
+            : base("DefaultConnection")
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MyDb"/> class.
+        /// </summary>
+        /// <param name="connectionString">The connection string.</param>
+        /// <param name="providerName">Name of the provider.</param>
+        public MyDb(string connectionString, string providerName)
+            : base(connectionString, providerName)
+        {
+        }
+    }
+}
---
+++ SQLiteTester/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NPoco;
+
+namespace SQLiteTester
+{
+    /// <summary>
+    /// Class User
+    /// </summary>
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    /// <summary>
+    /// Class Program
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = ConfigurationManager.ConnectionStrings["DefaultConnection"];
+            var db = new MyDb(config.ConnectionString, config.ProviderName);
+            List<User> users = db.Fetch<User>(string.Empty);
+            Console.WriteLine(users.Count);
+        }
+    }
+}
